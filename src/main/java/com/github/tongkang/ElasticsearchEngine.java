2c2450b785921eb66f29c4fb7782e58e07992f57
tongkang <tongkang97@gmail.com>
2019-12-04 23:01:29 +0800

    构造一个简单的Elasticsearch搜索

---
+++ src/main/java/com/github/tongkang/ElasticsearchEngine.java
@@ -0,0 +1,41 @@
+package com.github.tongkang;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ElasticsearchEngine {
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("Please input a search keyword:");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String keyword = reader.readLine();
+            search(keyword);
+        }
+    }
+
+    private static void search(String keyword) throws IOException {
+        try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
+            SearchRequest request = new SearchRequest("news");
+            request.source(new SearchSourceBuilder().query(new MultiMatchQueryBuilder(keyword, "title", "content")));
+
+            SearchResponse result = client.search(request, RequestOptions.DEFAULT);
+
+            for (SearchHit hit : result.getHits()) {
+                System.out.println(hit.getSourceAsString());
+            }
+
+        }
+    }
+
+}
